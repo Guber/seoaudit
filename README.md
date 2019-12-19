735bd90747053ddde12b4ba3897219b89484e440
Emanuel Guberovic <emanuel.guberovic@gmail.com>
2019-12-19 22:07:52 +0100

    - initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# SEOAudit
+
+SEO tool that analyzer a set of urls, crawls the pages and audits a set of predefiend checks element, page and site
+ tests on them.
+
+## Requirements
+
+Python 3.6+ and modules 'requests', 'lxml', 'nltk', 'extruct', 'selenium'
+
+## Installation
+
+pip install
--- setup.py
+++ setup.py
@@ -38,9 +38,9 @@ EXTRAS = {
 here = os.path.abspath(os.path.dirname(__file__))
 
 # Import the README and use it as the long-description.
-# Note: this will only work if 'README.txt' is present in your MANIFEST.in file!
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
 try:
-    with io.open(os.path.join(here, 'README.txt'), encoding='utf-8') as f:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
         long_description = '\n' + f.read()
 except FileNotFoundError:
     long_description = DESCRIPTION
@@ -80,7 +80,8 @@ class UploadCommand(Command):
             pass
 
         self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+        #os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+        os.system('setup.py sdist bdist')
 
         self.status('Uploading the package to PyPI via Twine…')
         os.system('twine upload dist/*')
